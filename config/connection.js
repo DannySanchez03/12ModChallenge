13c65a537e4e679af20ceaf33b6e611a7c7aece4
DannySanchez03 <dannyproa303@gmail.com>
2024-03-08 23:26:15 -0600

    added final index.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 const mysql = require('mysql2');
 require('dotenv').config();
-const mysqlPassword = process.env.MYSQL_PASSWORD;
-const connection = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: mysqlPassword,
-        database: 'employee_db'
-    },
-    console.log('Connected to the database.')
-);
+const Password = process.env.DB_PASSWORD;
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: Password,
+    database: 'employee_db'
+});
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        return;
+    }
+    console.log('Connected to the database.');
+});
 module.exports = connection;
---
+++ index.js
@@ -0,0 +1,47 @@
+const inquirer = require('inquirer');
+const { viewAllDepartments, viewAllRoles, viewAllEmployees, addDepartment, addRole, addEmployee, updateRole } = require('./queries/queries.js')
+
+const question = [
+    {
+        type: 'list',
+        name: 'dataOption',
+        message: 'What would you like to do?',
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit']
+    }
+]
+
+function init() {
+    inquirer.prompt(question)
+        .then((answers) => {
+            const choice = answers.dataOption
+            console.log(choice);
+
+            switch (choice) {
+                case 'View all departments':
+                    viewAllDepartments(init);
+                    break;
+                case 'View all roles':
+                    viewAllRoles(init);
+                    break;
+                case 'View all employees':
+                    viewAllEmployees(init);
+                    break;
+                case 'Add a department':
+                    addDepartment(init);
+                    break;
+                case 'Add a role':
+                    addRole(init);
+                    break;
+                case 'Add an employee':
+                    addEmployee(init);
+                    break;
+                case 'Update an employee role':
+                    updateRole(init);
+                    break;
+                default:
+                    process.exit(0);
+            }
+        });
+}
+
+init();
